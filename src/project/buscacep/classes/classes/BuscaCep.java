5662463556d09b1c56b6fd13701567beed72c9e7
vitorzanela <vitor.mardig@gmail.com>
2025-06-23 00:41:20 -0300

    Corrigido a variavel <estado> presenta no record BuscaCep, para funcionamento correto da aplicação

--- src/project/buscacep/classes/classes/BuscaCep.java
+++ src/project/buscacep/classes/classes/BuscaCep.java
@@ -1,13 +1,13 @@
 package project.buscacep.classes.classes;
 
-public record BuscaCep(String cep, String logradouro, String complemento, String bairro, String uf, String estado, String regiao, String ddd) {
+public record BuscaCep(String cep, String logradouro, String complemento, String bairro, String uf, String localidade, String regiao, String ddd) {
     @Override
     public String toString() {
         return "----RESULTADO----\n" +
                 "Logradouro: " + logradouro +
                 "\nCOMPL: " + complemento +
                 "\nBairro: " + bairro +
-                "\nCidade: " + estado +
+                "\nCidade: " + localidade +
                 "\nCEP: " + cep +
                 "\nUF: " + uf ;
     }
--- src/project/buscacep/classes/main/Main.java
+++ src/project/buscacep/classes/main/Main.java
@@ -6,7 +6,6 @@
     import project.buscacep.classes.classes.ServiceBuscaEnd;
     
     import java.io.IOException;
-    import java.util.Arrays;
     
     public class Main {
         public static void main(String[] args) throws IOException, InterruptedException {
